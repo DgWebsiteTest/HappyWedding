ca9b167a16a0136c9b894e6cfeb827fcdf871ef4
DgWebsiteTest <dgmy100@gmail.com>
2025-12-23 19:39:50 +0530

    Add files via upload

---
+++ jquerycode.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('#menu').slicknav({
+        'label':'',
+        'brand':'DG',
+    });
+});
+
+$(document).ready(function() {
+    $(".skitter-large").skitter({
+        dots:true,
+        navigation:true,
+        theme:"square"
+    });
+  });
